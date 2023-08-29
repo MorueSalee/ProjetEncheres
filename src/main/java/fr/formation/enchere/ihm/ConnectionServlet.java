f9616a74ccc663a2f9cf4c1e3eaea5849b1142c5
ylaunay2022 <ylaunay2022@44SE34-JQV6YW2.ad.campus-eni.fr>
2023-08-29 14:30:31 +0200

    Servlet connection, logout, register

--- src/main/java/fr/formation/enchere/ihm/ConnectionServlet.java
+++ src/main/java/fr/formation/enchere/ihm/ConnectionServlet.java
@@ -7,11 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import fr.formation.enchere.bll.UtilisateurManager;
+import fr.formation.enchere.bll.UtilisateurManagerSing;
+import fr.formation.enchere.bo.Utilisateur;
+
 /**
  * Servlet implementation class ConnectionServlet
  */
 public class ConnectionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private UtilisateurManager manager = UtilisateurManagerSing.getInstance();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -32,8 +37,28 @@ public class ConnectionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String pseudo = request.getParameter("pseudo");
+		String motDePasse = request.getParameter("motDePasse");
 		
+		Utilisateur utilisateur = manager.check(pseudo, motDePasse);
+		if(utilisateur==null) {
+			request.setAttribute("message", "utilisteur inconnu");
+			request.getRequestDispatcher("/WEB-INF/connection.jsp").forward(request, response);
+		}
+		else {
+			// On met l'utilisateur en session
+			request.getSession().setAttribute("utilisateur", utilisateur);
+
+			// On appelle l'url de la servlet initialisement appelée
+			String urlPattern = (String) request.getSession().getAttribute("urlPattern");
+
+			// Si pas de servlet initialement appelée on va sur l'url racine (ou il y a généralement le menu
+			if(urlPattern==null) {
+				urlPattern="/";
+			}
+			
+			request.getRequestDispatcher(urlPattern).forward(request, response);
+		}
 	}
 
 }
