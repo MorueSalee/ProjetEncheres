179138f0ca55db5b202094edb18842028e973597
fpereira2022 <fpereira2022@44SE34-CSV6YW2.ad.campus-eni.fr>
2023-08-29 14:29:33 +0200

    Finished SQL query for Utilisateur DAL

--- src/main/java/fr/formation/enchere/bll/UtilisateurManagerImpl.java
+++ src/main/java/fr/formation/enchere/bll/UtilisateurManagerImpl.java
@@ -3,11 +3,13 @@ package fr.formation.enchere.bll;
 import java.util.List;
 
 import fr.formation.enchere.bo.Utilisateur;
+import fr.formation.enchere.dal.DALException;
 import fr.formation.enchere.dal.UtilisateurDAO;
+import fr.formation.enchere.dal.UtilisateurDAOFact;
 
 public class UtilisateurManagerImpl implements UtilisateurManager {
 
-private UtilisateurDAO dao = UtilisateurDAOFact.getUserDAO();
+private UtilisateurDAO dao = UtilisateurDAOFact.getUtilisateurDAO();
 	
 	public void addUtilisateur(Utilisateur utilisateur) {
 	    // Vérifier l'unicité du pseudo et de l'email avant d'ajouter l'utilisateur
@@ -20,28 +22,56 @@ private UtilisateurDAO dao = UtilisateurDAOFact.getUserDAO();
 	        throw new IllegalArgumentException("Le pseudo ne doit contenir que des caractères alphanumériques.");
 	    }
 	
-	    dao.insert(utilisateur);
+	    try {
+			dao.insert(utilisateur);
+		} catch (DALException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Utilisateur check(String pseudo, String motDePasse) {
-	    List<Utilisateur> users = dao.findByLoginAndPassword(pseudo, motDePasse);
-	    if (!users.isEmpty()) {
-	        return users.get(0);
-	    } else {
-	        return null;
-	    }
+	    List<Utilisateur> users;
+		try {
+			users = dao.findByLoginAndPassword(pseudo, motDePasse);
+			
+			if (!users.isEmpty()) {
+		        return users.get(0);
+		    }
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return null;
 	}
 	
 	private boolean isPseudoUnique(String pseudo) {
 	    // Vérifier si le pseudo est unique dans la base de données
-	    List<Utilisateur> usersWithPseudo = dao.findByPseudo(pseudo);
-	    return usersWithPseudo.isEmpty();
+	    List<Utilisateur> usersWithPseudo;
+	    Boolean result = false;
+	    
+		try {
+			usersWithPseudo = dao.findByPseudo(pseudo);
+			result = usersWithPseudo.isEmpty();
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return result;
 	}
 	
 	private boolean isEmailUnique(String email) {
 	    // Vérifier si l'email est unique dans la base de données
-	    List<Utilisateur> usersWithEmail = dao.findByEmail(email);
-	    return usersWithEmail.isEmpty();
+	    List<Utilisateur> usersWithEmail;
+	    Boolean result = false;
+	    
+		try {
+			usersWithEmail = dao.findByEmail(email);
+			result = usersWithEmail.isEmpty();
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return result;
 	}
 
 }
--- src/main/java/fr/formation/enchere/dal/UtilisateurDAO.java
+++ src/main/java/fr/formation/enchere/dal/UtilisateurDAO.java
@@ -7,10 +7,11 @@ import fr.formation.enchere.bo.Utilisateur;
 public interface UtilisateurDAO {
 	
 	public void insert(Utilisateur article) throws DALException;
-	public Utilisateur getById(Integer id) throws DALException;
+	public Utilisateur findById(Integer id) throws DALException;
 	//public void getAll() throws DALException;
 	public void delete(Integer id) throws DALException;
 	public void update(Utilisateur article) throws DALException;
-	public List<Utilisateur> findByLoginAndPassword(String pseudo, String motDePasse);
-	
+	public List<Utilisateur> findByLoginAndPassword(String pseudo, String motDePasse) throws DALException;
+	public List<Utilisateur> findByPseudo(String pseudo) throws DALException;
+	public List<Utilisateur> findByEmail(String email) throws DALException;
 }
